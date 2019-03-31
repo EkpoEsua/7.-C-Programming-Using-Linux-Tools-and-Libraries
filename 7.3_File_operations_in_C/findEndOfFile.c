ea1c1fa5cdf246a1bddeffb55fbce1f781c409ce
EkpoEsua <esuaekpo@yahoo.com>
2019-03-31 20:56:36 +0100

    Added code on Finding the end of a File:  findEndOfFile.c

---
+++ 7.3_File_operations_in_C/findEndOfFile.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main(){
+    FILE *file;
+
+    double myStudents, otherStudents;
+    int best = 1;
+    int count = 1;
+
+    file = fopen("gradeComparison.txt", "r");
+
+    fscanf(file, "%lf", &myStudents);
+
+    while ((fscanf(file, "%lf", &otherStudents) != EOF) && best){
+        count += 1;
+        if (myStudents < otherStudents) best = 0;
+    }
+
+    if (best) printf("Yes");
+    else printf("No %d", count);
+
+
+    return 0;
+}
