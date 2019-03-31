885f8101d5110877dadfc148327ed73b4879abca
EkpoEsua <esuaekpo@yahoo.com>
2019-03-31 12:53:13 +0100

    Changes
    New files added

---
+++ 7.3_File_operations_in_C/readNumbersFromAFile.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(){
+    int N, num;
+    double total = 0.0;
+    FILE *ifile;
+    
+    ifile = fopen("studentGrades.txt", "r");
+    
+    fscanf(ifile, "%d", &N);
+    
+    for( int i = 0; i < N; i++){
+        fscanf(ifile, "%d", &num);
+        total += num;
+    }
+    
+    printf("%.2lf", total/N);
+    
+    return 0;
+}
